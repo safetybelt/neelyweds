960b77537d994b00dfa3c4ebad6d4633283c4856
Jonathan Neely <jneely@gmail.com>
2017-05-15 17:32:37 -0700

    Added more JSON reader support

--- src/utils/json-reader.js
+++ src/utils/json-reader.js
@@ -5,6 +5,8 @@ export function getLayoutFromJson(json) {
         if (elem.hasOwnProperty('image')) {
             const img = require(`img/${elem.image}`);
             return (<img key={idx} src={img} />);             // why the fuck is this auto-indenting without the parentheses?
+        } else if (elem.hasOwnProperty('text')) {
+            return <p key={idx}>{elem.text}</p>;
         } else {
             return <p key={idx}>{elem}</p>;
         }
